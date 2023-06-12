ec074c9a39d9fee511b3e0760015e42c78d3dedc
Marat252552 <117304418+Marat252552@users.noreply.github.com>
2023-06-12 21:14:05 +0300

    0.2

--- src/configs/NavigationConfig.js
+++ src/configs/NavigationConfig.js
@@ -1,10 +1,21 @@
-import { 
-  DashboardOutlined
+import {
+  DashboardOutlined,
+  ShoppingCartOutlined,
+  ShoppingOutlined,
+  UserOutlined,
+  PictureOutlined,
+  GiftOutlined,
+  ShopOutlined,
+  UsergroupAddOutlined,
+  MailOutlined,
+  SettingOutlined,
+  MobileOutlined,
+  FileTextOutlined
 } from '@ant-design/icons';
 import { APP_PREFIX_PATH } from 'configs/AppConfig'
 
-const dashBoardNavTree = [{
-  key: 'general',
+const generalsNavTree = [{
+  key: 'generals',
   path: `${APP_PREFIX_PATH}/generals`,
   title: 'sidenav.generals',
   icon: DashboardOutlined,
@@ -19,76 +30,178 @@ const dashBoardNavTree = [{
       submenu: []
     },
     {
-    key: 'catalog',
-    path: `${APP_PREFIX_PATH}/generals/catalog`,
-    title: 'sidenav.generals.catalog',
-    icon: DashboardOutlined,
-    breadcrumb: false,
-    submenu: []
-  },
-  {
-    key: 'orders',
-    path: `${APP_PREFIX_PATH}/generals/orders`,
-    title: 'sidenav.generals.orders',
-    icon: DashboardOutlined,
-    breadcrumb: false,
-    submenu: []
-  },
-  {
-    key: 'clients',
-    path: `${APP_PREFIX_PATH}/generals/clients`,
-    title: 'sidenav.generals.clients',
-    icon: DashboardOutlined,
-    breadcrumb: false,
-    submenu: []
-  },
-  {
-    key: 'banners',
-    path: `${APP_PREFIX_PATH}/generals/banners`,
-    title: 'sidenav.generals.banners',
-    icon: DashboardOutlined,
-    breadcrumb: false,
-    submenu: []
-  },
-  {
-    key: 'promocodes',
-    path: `${APP_PREFIX_PATH}/generals/promocodes`,
-    title: 'sidenav.generals.promocodes',
-    icon: DashboardOutlined,
-    breadcrumb: false,
-    submenu: []
-  },
-  {
-    key: 'offline_places',
-    path: `${APP_PREFIX_PATH}/generals/offline_places`,
-    title: 'sidenav.generals.offline_places',
-    icon: DashboardOutlined,
-    breadcrumb: false,
-    submenu: []
-  },
-  {
-    key: 'employess',
-    path: `${APP_PREFIX_PATH}/generals/employees`,
-    title: 'sidenav.generals.employees',
-    icon: DashboardOutlined,
-    breadcrumb: false,
-    submenu: []
-  },
-  {
-    key: 'distribution',
-    path: `${APP_PREFIX_PATH}/generals/distribution`,
-    title: 'sidenav.generals.distribution',
-    icon: DashboardOutlined,
-    breadcrumb: false,
-    submenu: []
-  }]
+      key: 'catalog',
+      path: `${APP_PREFIX_PATH}/generals/catalog`,
+      title: 'sidenav.generals.catalog',
+      icon: ShoppingCartOutlined,
+      breadcrumb: false,
+      submenu: [{
+        key: 'items',
+        path: `${APP_PREFIX_PATH}/generals/catalog/items`,
+        title: 'sidenav.generals.catalog.items',
+        icon: '',
+        breadcrumb: false,
+        submenu: []
+      },
+      {
+        key: 'categories',
+        path: `${APP_PREFIX_PATH}/generals/catalog/categories`,
+        title: 'sidenav.generals.catalog.categories',
+        icon: '',
+        breadcrumb: false,
+        submenu: []
+      },
+      {
+        key: 'collections',
+        path: `${APP_PREFIX_PATH}/generals/catalog/collections`,
+        title: 'sidenav.generals.catalog.collections',
+        icon: '',
+        breadcrumb: false,
+        submenu: []
+      },
+      {
+        key: 'combo',
+        path: `${APP_PREFIX_PATH}/generals/catalog/combo`,
+        title: 'sidenav.generals.catalog.combo',
+        icon: '',
+        breadcrumb: false,
+        submenu: []
+      }
+    ]
+    },
+    {
+      key: 'orders',
+      path: `${APP_PREFIX_PATH}/generals/orders`,
+      title: 'sidenav.generals.orders',
+      icon: ShoppingOutlined,
+      breadcrumb: false,
+      submenu: []
+    },
+    {
+      key: 'clients',
+      path: `${APP_PREFIX_PATH}/generals/clients`,
+      title: 'sidenav.generals.clients',
+      icon: UserOutlined,
+      breadcrumb: false,
+      submenu: [
+        {
+          key: 'clients_list',
+          path: `${APP_PREFIX_PATH}/generals/clients/clients_list`,
+          title: 'sidenav.generals.clients.clients_list',
+          icon: '',
+          breadcrumb: false,
+          submenu: []
+        },
+        {
+          key: 'clients_groups',
+          path: `${APP_PREFIX_PATH}/generals/clients/clients_groups`,
+          title: 'sidenav.generals.clients.clients_groups',
+          icon: '',
+          breadcrumb: false,
+          submenu: []
+        }
+      ]
+    },
+    {
+      key: 'banners',
+      path: `${APP_PREFIX_PATH}/generals/banners`,
+      title: 'sidenav.generals.banners',
+      icon: PictureOutlined,
+      breadcrumb: false,
+      submenu: []
+    },
+    {
+      key: 'promocodes',
+      path: `${APP_PREFIX_PATH}/generals/promocodes`,
+      title: 'sidenav.generals.promocodes',
+      icon: GiftOutlined,
+      breadcrumb: false,
+      submenu: []
+    },
+    {
+      key: 'offline_places',
+      path: `${APP_PREFIX_PATH}/generals/offline_places`,
+      title: 'sidenav.generals.offline_places',
+      icon: ShopOutlined,
+      breadcrumb: false,
+      submenu: [
+        {
+          key: 'adresses',
+          path: `${APP_PREFIX_PATH}/generals/offline_places/adresses`,
+          title: 'sidenav.generals.offline_places.adresses',
+          icon: '',
+          breadcrumb: false,
+          submenu: []
+        },
+        {
+          key: 'geozones',
+          path: `${APP_PREFIX_PATH}/generals/offline_places/geozones`,
+          title: 'sidenav.generals.offline_places.geozones',
+          icon: '',
+          breadcrumb: false,
+          submenu: []
+        },
+      ]
+    },
+    {
+      key: 'employess',
+      path: `${APP_PREFIX_PATH}/generals/employees`,
+      title: 'sidenav.generals.employees',
+      icon: UsergroupAddOutlined,
+      breadcrumb: false,
+      submenu: []
+    },
+    {
+      key: 'distribution',
+      path: `${APP_PREFIX_PATH}/generals/distribution`,
+      title: 'sidenav.generals.distribution',
+      icon: MailOutlined,
+      breadcrumb: false,
+      submenu: []
+    }]
 },
 
 
 ]
+const systemsNavTree = [{
+  key: 'systems',
+  path: `${APP_PREFIX_PATH}/systems`,
+  title: 'sidenav.systems',
+  icon: DashboardOutlined,
+  breadcrumb: false,
+  submenu: [
+    {
+      key: 'settings',
+      path: `${APP_PREFIX_PATH}/systems/settings`,
+      title: 'sidenav.systems.settings',
+      icon: SettingOutlined,
+      breadcrumb: false,
+      submenu: []
+    },
+    {
+      key: 'mobile_app',
+      path: `${APP_PREFIX_PATH}/systems/mobile_app`,
+      title: 'sidenav.systems.mobile_app',
+      icon: MobileOutlined,
+      breadcrumb: false,
+      submenu: []
+    },
+    {
+      key: 'logs',
+      path: `${APP_PREFIX_PATH}/systems/logs`,
+      title: 'sidenav.systems.logs',
+      icon: FileTextOutlined,
+      breadcrumb: false,
+      submenu: []
+    },
+    ]
+},
+]
 
 const navigationConfig = [
-  ...dashBoardNavTree
+  ...generalsNavTree,
+  ...systemsNavTree
 ]
+
 
 export default navigationConfig;
--- src/views/app-views/index.js
+++ src/views/app-views/index.js
@@ -8,11 +8,7 @@ export const AppViews = () => {
     <Suspense fallback={<Loading cover="content"/>}>
       <Switch>
         <Route path={`${APP_PREFIX_PATH}/generals`} component={lazy(() => import(`./generals`))} />
-        {/* <Route path={`${APP_PREFIX_PATH}/banners`} component={lazy(() => import(`./banners`))} />
-        <Route path={`${APP_PREFIX_PATH}/clients`} component={lazy(() => import(`./clients`))} />
-        <Route path={`${APP_PREFIX_PATH}/orders`} component={lazy(() => import(`./orders`))} />
-        <Route path={`${APP_PREFIX_PATH}/banners`} component={lazy(() => import(`./banners`))} />
-        <Route path={`${APP_PREFIX_PATH}/catalog`} component={lazy(() => import(`./catalog`))} /> */}
+        <Route path={`${APP_PREFIX_PATH}/systems`} component={lazy(() => import(`./systems`))} />
         <Redirect from={`${APP_PREFIX_PATH}`} to={`${APP_PREFIX_PATH}/generals`} />
       </Switch>
     </Suspense>
